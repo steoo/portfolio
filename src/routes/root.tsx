68fd09c51d3aa10d7226397b58d0254fd8e94287
steoo <esseiproject@gmail.com>
2025-04-10 16:29:09 +0200

    text

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -22,7 +22,7 @@ const Root = () => {
       <Footer>
         <MenuItem to="/home">stefano imparato</MenuItem>
         <a href="https://github.com/steoo/portfolio" target="_blank">
-          you can find thecode on Github
+          you can find the code on Github
         </a>
       </Footer>
     </RootContainer>
